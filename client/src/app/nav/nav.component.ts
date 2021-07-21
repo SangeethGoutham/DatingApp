0767833f6c3cce691a7024caabc9ccfe329538dc
Sangeeth Goutham <sangeethgoutham@outlook.com>
2021-07-21 16:35:27 +0530

    21-07-2021 Client Login and register

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,17 +10,18 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn: boolean;
+  
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
+    
   }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
+      
     }, 
     error=>{console.log("Error")}
     )
@@ -26,7 +29,8 @@ export class NavComponent implements OnInit {
 
   logout()
   {
-    this.loggedIn = false;
+    this.accountService.logout();
+    
   }
 
 }
